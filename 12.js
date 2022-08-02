51c8742b7e6f753b0611101601e16811ebeff756
David Erazo <da.erazom@gmail.com>
2022-08-02 02:10:58 -0400

    refactor

--- 12.js
+++ 12.js
@@ -1,19 +1,17 @@
 function getMinJump(obstacles) {
   let candidate = 0;
-  let max = 0;
+  let max = Math.max(...obstacles);
   
-  for (let i = 0; i<obstacles.length; i++) {
-    console.log(i);
-    if (obstacles[i] > max) {
-      max = obstacles[i];
-    }
-  }
+  const obstaclesMap = obstacles.reduce((acc, value) => {
+    acc[value] = value;
+    return acc;
+  }, {});  
 
   for (let i = 1; i<=max; i++) {
     let parcialCandidate = 0;
     
     for (let j=i; j<=max; j = j + i) {
-      if(!obstacles.includes(j)) {
+      if(!obstaclesMap[j]) {
         parcialCandidate = i;
       } else {
         parcialCandidate = 0;
@@ -42,3 +40,5 @@ getMinJump([3, 7, 5]) // -> 2
 getMinJump([9, 5, 1]) // -> 2
 
 getMinJump([14, 10, 8, 2, 3, 6])
+//.     x. x.       x     x      x.              x  
+//0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
