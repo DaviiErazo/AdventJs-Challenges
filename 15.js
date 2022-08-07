4c1dc2977515fa20264c885f9dd337cfccdd5e98
David Erazo <da.erazom@gmail.com>
2022-08-07 00:31:26 -0400

    15 ok

---
+++ 15.js
@@ -0,0 +1,39 @@
+const hasNotUps = (position, maxPositions) => {
+  return position != 0 && maxPositions.position == 0;
+}
+const nextValuesIsEqual = (nextHeight, height) => nextHeight == height;
+const hasDowns = (nextHeight, height) => nextHeight < height;
+
+function checkSledJump(heights) {
+  // ¡No olvides compartir tu solución en redes!
+  let max = { position: 0, value: 0 };
+  let downs = false;
+  
+  for (let i=0; i<heights.length; i++) {
+    const height = heights[i];
+    const nextHeight = heights[i + 1];
+    
+		if (hasNotUps(i, max)) return false;
+    if (nextValuesIsEqual(nextHeight, height)) return false;
+    if (hasDowns(nextHeight, height)) downs = true;
+    
+    if (nextHeight > height && i - max.position == 0) {
+			if () {
+        max.position = i + 1;
+        max.value = nextHeight;
+      } else {
+        return false;
+      }
+    }
+  }
+  return downs
+}
+
+checkSledJump([1, 2, 3, 2, 1]) // true: sube y baja de forma estricta
+checkSledJump([0, 1, 0]) // -> true: sube y baja de forma estricta
+checkSledJump([0, 3, 2, 1]) // -> true: sube y baja de forma estricta
+checkSledJump([0, 1000, 1]) // -> true: sube y baja de forma estricta
+
+checkSledJump([2, 4, 4, 6, 2]) // false: no sube de forma estricta
+checkSledJump([1, 2, 3]) // false: sólo sube
+checkSledJump([1, 2, 3, 2, 1, 2, 3]) // false: sube y baja y sube... ¡no vale!
