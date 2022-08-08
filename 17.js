1aedbe2604af607bda9c3d42734a6b9726151121
David Erazo <da.erazom@gmail.com>
2022-08-07 20:42:05 -0400

    refactor 17

--- 17.js
+++ 17.js
@@ -1,22 +1,19 @@
 function countPackages(carriers, carrierID) {
 	let count = 0;
   
-  for (let i=0; i<carriers.length; i++) {
-    const carrier = carriers[i];
-    const carrierIdName = carrier[0];
-    const packages = carrier[1];
-    const team = carrier[3];
+  const carrier = carriers.find(carrier => carrier[0] === carrierID);
+  
+  const carrierIdName = carrier[0];
+  const packages = carrier[1];
+  const team = carrier[2];
     
-    if (carrierIdName == carrierID) {
-      count =+ packages;
-      if (team && team.length > 0) {
-        for (let j=0; j<team.length; j++) {
-          const coworker = team[j];
-          console.log(coworker);
-        }
-      }
-    }
-  }
+  count =+ packages;
+  
+  for (let j=0; j<team.length; j++) {
+    const coworker = team[j];
+    count += countPackages(carriers, coworker);
+	}
+  
   return count;
 }
 
@@ -29,3 +26,17 @@ const carriers = [
 
 countPackages(carriers, 'dapelu') // 9
 // 5 de dapelu, 2 de midu y 2 de jelowing = 9
+
+
+const carriers2 = [
+  ['lolivier', 8, ['camila', 'jesuspoleo']],
+  ['camila', 5, ['sergiomartinez', 'conchaasensio']],
+  ['jesuspoleo', 4, []],
+  ['sergiomartinez', 4, []],
+  ['conchaasensio', 3, ['facundocapua', 'faviola']],
+  ['facundocapua', 2, []],
+  ['faviola', 1, []]
+]
+
+countPackages(carriers2, 'camila') // 15
+// 5 de camila, 4 de sergiomartinez, 3 de conchaasensio, 2 de facundocapua y 1 de faviola = 15
