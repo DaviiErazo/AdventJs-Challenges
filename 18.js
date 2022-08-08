a662d01bc165cf4950f2c434efac6df2dfe9ca34
David Erazo <da.erazom@gmail.com>
2022-08-07 22:17:23 -0400

    18 ok

---
+++ 18.js
@@ -0,0 +1,19 @@
+
+function fixFiles(files) {
+  const filesCountMap = {};
+    
+ return files.map(file => {
+    filesCountMap[files] = filesCountMap[file] + 1 || 0;
+    return filesCountMap[file] ? `${file}(${filesCountMap[file]})` : file;
+  });
+}
+
+const files = ['photo', 'postcard', 'photo', 'photo', 'video']
+fixFiles(files) // ['photo', 'postcard', 'photo(1)', 'photo(2)', 'video']
+
+const files2 = ['file', 'file', 'file', 'game', 'game']
+fixFiles(files2) // ['file', 'file(1)', 'file(2)', 'game', 'game(1)']
+
+// ojo que los elfos ya tenían archivos con (1)... ¡y pueden estar repetidos!
+const files3 = ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)']
+fixFiles(files3) // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
